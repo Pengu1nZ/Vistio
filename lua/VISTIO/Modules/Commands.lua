bc28d79d9543f1fa3fbc84ca219a15cc23e16410
Liam-MacKay <guitarworld22@gmail.com>
2014-02-14 12:55:13 -0500

    Added all module files

---
+++ lua/VISTIO/Modules/Commands.lua
@@ -0,0 +1,23 @@
+local MODULE = {}
+MODULE.Name = "Commands"
+
+function MODULE.Init()
+	MODULE.LoadCommands()
+end
+
+function MODULE.RegisterCommand(t)
+	VISTIO.Commands[string.upper(t.Name)] = t
+end
+
+function MODULE.LoadCommand(s)
+	include("VISTIO/Commands/"..s)
+end
+
+function MODULE.LoadCommands()
+	VISTIO.Commands = {}
+	local files = file.Find("VISTIO/Commands/*.lua", "LUA")
+	for k, v in pairs(files) do
+		MODULE.LoadCommand(v)
+	end
+end
+
